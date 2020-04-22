21143272a6d73a7cfd3567302d60b8f5b15eb198
Jonah Mallard <benjaminjmallard@gmail.com>
2020-04-22 16:22:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
-  words = string.split('')
+  words = string.split(' ')
     reversed_words = []
       words.collect do |string|
         reversed_words<< string.reverse 
